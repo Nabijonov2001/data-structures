975c194e4ea5a99e4b47a136c88cac8384a4afa1
Fazliddin Nabijonov <fazliddinnabijonov6@gmail.com>
2025-05-01 17:52:02 +0500

    changed

---
+++ lesson4/quick-sort.js
@@ -0,0 +1,25 @@
+// quick sort
+
+const arr = [5, 3, 8, 4, 2, 12, 4, 22, 54];
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === Math.floor(arr.length / 2)) continue;
+
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
